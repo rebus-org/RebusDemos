ba9ab2b0652f9e26be5bf00dfda1e4b587f4e3da
Mogens Heller Grabe <mookid8000@gmail.com>
2013-01-13 21:16:19 +0100

    implemented saga thingie

--- Demo4/Billing/ChargeTheCustomer.cs
+++ Demo4/Billing/ChargeTheCustomer.cs
@@ -5,20 +5,34 @@ using Trading.Messages;
 
 namespace Billing
 {
-    class BillTheCustomer : Saga<BillingSagaData>, 
+    class ChargeTheCustomer : Saga<BillingSagaData>, 
         IAmInitiatedBy<NewTradeRecorded>,
         IAmInitiatedBy<CounterpartConfirmed>,
-        IAmInitiatedBy<CounterpartRejected>
+        IAmInitiatedBy<CounterpartRejected>,
+        IHandleMessages<VerifyComplete>
     {
+        readonly IBus bus;
+
+        public ChargeTheCustomer(IBus bus)
+        {
+            this.bus = bus;
+        }
+
         public override void ConfigureHowToFindSaga()
         {
             Incoming<NewTradeRecorded>(m => m.Counterpart).CorrelatesWith(d => d.Counterpart);
             Incoming<CounterpartConfirmed>(m => m.Counterpart).CorrelatesWith(d => d.Counterpart);
             Incoming<CounterpartRejected>(m => m.Counterpart).CorrelatesWith(d => d.Counterpart);
+            Incoming<VerifyComplete>(m => m.CorrelationId).CorrelatesWith(d => d.Id);
         }
 
         public void Handle(NewTradeRecorded message)
         {
+            if (IsNew)
+            {
+                bus.Defer(TimeSpan.FromSeconds(10), new VerifyComplete {CorrelationId = Data.Id});
+            }
+
             Data.Counterpart = message.Counterpart;
 
             Data.Amount = message.Amount;
@@ -35,6 +49,11 @@ namespace Billing
 
         public void Handle(CounterpartConfirmed message)
         {
+            if (IsNew)
+            {
+                bus.Defer(TimeSpan.FromSeconds(10), new VerifyComplete { CorrelationId = Data.Id });
+            }
+
             Data.Counterpart = message.Counterpart;
 
             Data.GotCreditStatus = true;
@@ -47,6 +66,11 @@ namespace Billing
 
         public void Handle(CounterpartRejected message)
         {
+            if (IsNew)
+            {
+                bus.Defer(TimeSpan.FromSeconds(10), new VerifyComplete { CorrelationId = Data.Id });
+            }
+
             Data.Counterpart = message.Counterpart;
 
             Data.GotCreditStatus = true;
@@ -69,7 +93,7 @@ namespace Billing
     credit:      {1}
     
     sending bill!!
-=========================", Data.Counterpart);
+=========================", Data.Counterpart, Data.CreditOk ? "OK" : "NOT OK");
             }
             else
             {
@@ -78,12 +102,30 @@ namespace Billing
     credit:      {1}
     
     u y no have moneys?!
-=========================", Data.Counterpart);
+=========================", Data.Counterpart, Data.CreditOk ? "OK" : "NOT OK");
             }
             Console.WriteLine(@"=========================");
 
             MarkAsComplete();
         }
+
+        public void Handle(VerifyComplete message)
+        {
+            Console.WriteLine(
+                @"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Oh noes!!!11
+
+The saga for {0} was not completed within timeout! 
+
+Now we probably want to send an email or something...
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+                Data.Counterpart);
+        }
+    }
+
+    class VerifyComplete
+    {
+        public Guid CorrelationId { get; set; }
     }
 
     class BillingSagaData : ISagaData
--- Demo4/Billing/Program.cs
+++ Demo4/Billing/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Confirmations.Messages;
 using Rebus.Configuration;
+using Rebus.Logging;
 using Rebus.Transports.Msmq;
 using Trading.Messages;
-using Rebus.Logging;
+using Rebus.MongoDb;
 
 namespace Billing
 {
@@ -12,18 +14,23 @@ namespace Billing
         {
             using (var adapter = new BuiltinContainerAdapter())
             {
-                adapter.Register(typeof (BillTheCustomer));
+                adapter.Register(() => new ChargeTheCustomer(adapter.Bus));
 
                 Configure.With(adapter)
                          .Logging(l => l.ColoredConsole(LogLevel.Error))
                          .Transport(t => t.UseMsmqAndGetInputQueueNameFromAppConfig())
                          .MessageOwnership(o => o.FromRebusConfigurationSection())
+                         .Sagas(s => s.StoreInMongoDb("mongodb://localhost/billing")
+                                      .SetCollectionName<BillingSagaData>("billingSagas"))
+                         .Timeouts(t => t.StoreInMongoDb("mongodb://localhost/billing", "timeouts"))
                          .CreateBus()
                          .Start();
 
                 Console.WriteLine("----Billing----");
 
                 adapter.Bus.Subscribe<NewTradeRecorded>();
+                adapter.Bus.Subscribe<CounterpartConfirmed>();
+                adapter.Bus.Subscribe<CounterpartRejected>();
 
                 Console.WriteLine("Press ENTER to quit");
                 Console.ReadLine();
