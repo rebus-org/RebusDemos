f23b51f79f8be3a0423a09fe9adc3c751671bf9b
Mogens Heller Grabe <mookid8000@gmail.com>
2013-09-02 21:57:03 +0200

    new demo for showing off transports

---
+++ Demo4c/Server.Messages/GreetingMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Messages
+{
+    public class GreetingMessage
+    {
+        public GreetingMessage(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; private set; }
+    }
+}
