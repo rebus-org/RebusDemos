5e770b6e3a9e0c361c2e9879257cd8fa955eb83b
Mogens Heller Grabe <mookid8000@gmail.com>
2013-09-02 21:27:10 +0200

    added demo4b

---
+++ Demo4b/Server/Handlers/GreetingHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Rebus;
+
+namespace Server.Handlers
+{
+    public class GreetingHandler : IHandleMessages<string>
+    {
+        readonly IBus bus;
+
+        public GreetingHandler(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(string message)
+        {
+            Console.WriteLine("Sending reply to greeting containing {0} chars", message.Length);
+
+            bus.Reply(string.Format("Thank you for your message containing {0} chars", message.Length));
+        }
+    }
+}
