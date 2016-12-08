1e2cc49a0bf667c776bf030b7b45edd5d74bdd0c
Mogens Heller Grabe <mookid8000@gmail.com>
2016-12-08 15:47:40 +0100

    about to assess me some credits

---
+++ Demo2/ExternalCreditAssessor/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace ExternalCreditAssessor
+{
+    public static class Helpers
+    {
+        public static async Task WriteStatus(this IOwinContext context, HttpStatusCode status, string reason)
+        {
+            var intStatus = (int) status;
+            var response = context.Response;
+
+            response.ReasonPhrase = reason;
+            response.StatusCode = intStatus;
+
+            await response.WriteAsync($"HTTP {intStatus} ({status}): {reason}");
+        }
+    }
+}
--- Demo2/ExternalCreditAssessor/Program.cs
+++ Demo2/ExternalCreditAssessor/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace ExternalCreditAssessor
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace ExternalCreditAssessor
 {
     class Program
     {
         static void Main()
         {
+            using (WebApp.Start<Startup>("https://localhost:7000"))
+            {
+                Console.WriteLine("External credit assessment service started...");
+                Console.WriteLine("Press ENTER to quit");
+                Console.ReadLine();
+            }
+        }
+    }
 
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var checks = new ConcurrentDictionary<string, int>();
+            var getRandom = new ThreadLocal<Random>(() => new Random(DateTime.Now.GetHashCode()));
+
+            app.Map("/check-credit", a =>
+            {
+                a.Run(async context =>
+                {
+                    var request = context.Request;
+
+                    if (request.Method != "GET")
+                    {
+                        await context.WriteStatus(HttpStatusCode.BadRequest, "Must be GET");
+                    }
+
+                    var query = request.Query;
+                    var counterparty = query.Get("counterparty")?.ToLowerInvariant();
+
+                    if (string.IsNullOrWhiteSpace(counterparty))
+                    {
+                        await context.WriteStatus(HttpStatusCode.BadRequest, "Must include 'counterparty' form parameter");
+                        return;
+                    }
+
+                    var numberOfChecks = checks.AddOrUpdate(counterparty, 1, (c, n) => n + 1);
+                    var random = getRandom.Value.Next(numberOfChecks);
+
+
+                });
+            });
         }
     }
 }
+
