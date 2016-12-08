e213a81a1b161129059d173616b0fea09863ce88
Mogens Heller Grabe <mookid8000@gmail.com>
2016-12-08 14:46:56 +0100

    demo 0

--- Demo0/Client/Program.cs
+++ Demo0/Client/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace Client
+﻿using System;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+using Rebus.Routing.TypeBased;
+
+namespace Client
 {
     class Program
     {
         static void Main()
         {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                var bus = Configure.With(activator)
+                    .Logging(l => l.ColoredConsole(LogLevel.Warn))
+                    .Transport(t => t.UseMsmqAsOneWayClient())
+                    .Routing(t => t.TypeBased().Map<string>("server"))
+                    .Start();
+
+                while (true)
+                {
+                    Console.Write("> ");
+                    var text = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(text)) break;
+
+                    bus.Send(text);
+                }
+            }
         }
     }
 }
--- Demo0/Server/Program.cs
+++ Demo0/Server/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace Server
+﻿using System;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+
+#pragma warning disable 1998
+
+namespace Server
 {
     class Program
     {
         static void Main()
         {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                activator.Handle<string>(async message =>
+                {
+                    Console.WriteLine($"Received message {message}");
+                });
+
+                Configure.With(activator)
+                    .Logging(l => l.ColoredConsole(LogLevel.Warn))
+                    .Transport(t => t.UseMsmq("server"))
+                    .Start();
+
+                Console.WriteLine("Press ENTER to quit");
+                Console.ReadLine();
+            }
         }
     }
 }
