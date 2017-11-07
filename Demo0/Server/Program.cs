1231fd364788bbaf1ba32dcf9cb9e06c23933942
Mogens Heller Grabe <mookid8000@gmail.com>
2017-11-07 15:24:19 +0100

    add demo0

---
+++ Demo0/Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+// ReSharper disable ArgumentsStyleNamedExpression
+#pragma warning disable 1998
+
+namespace Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                activator.Handle<string>(async str =>
+                {
+                    Console.WriteLine($"Got greeting: {str}");
+                });
+
+                Configure.With(activator)
+                    .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
+                    .Transport(t => t.UseMsmq("server"))
+                    .Start();
+
+                Console.WriteLine("Press ENTER to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
