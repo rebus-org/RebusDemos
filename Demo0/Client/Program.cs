393b17b308a77c5d05177177669576672d1ee08f
Mogens Heller Grabe <mookid8000@gmail.com>
2018-08-21 22:22:11 +0200

    demo 0 ready

--- Demo0/Client/Program.cs
+++ Demo0/Client/Program.cs
@@ -1,34 +1,9 @@
-﻿using System;
-using Rebus.Activation;
-using Rebus.Config;
-using Rebus.Logging;
-using Rebus.Routing.TypeBased;
-// ReSharper disable ArgumentsStyleNamedExpression
-
-namespace Client
+﻿namespace Client
 {
     class Program
     {
         static void Main()
         {
-            using (var activator = new BuiltinHandlerActivator())
-            {
-                var bus = Configure.With(activator)
-                    .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
-                    .Transport(t => t.UseMsmq("client"))
-                    .Routing(r => r.TypeBased().Map<string>("server"))
-                    .Start();
-
-                while (true)
-                {
-                    Console.Write("Type greeting >");
-                    var text = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(text)) break;
-
-                    bus.Send(text).Wait();
-                }
-            }
 
         }
     }
--- Demo0/Server/Program.cs
+++ Demo0/Server/Program.cs
@@ -1,31 +1,9 @@
-﻿using System;
-using Rebus.Activation;
-using Rebus.Config;
-using Rebus.Logging;
-// ReSharper disable ArgumentsStyleNamedExpression
-#pragma warning disable 1998
-
-namespace Server
+﻿namespace Server
 {
     class Program
     {
         static void Main()
         {
-            using (var activator = new BuiltinHandlerActivator())
-            {
-                activator.Handle<string>(async str =>
-                {
-                    Console.WriteLine($"Got greeting: {str}");
-                });
-
-                Configure.With(activator)
-                    .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
-                    .Transport(t => t.UseMsmq("server"))
-                    .Start();
-
-                Console.WriteLine("Press ENTER to quit");
-                Console.ReadLine();
-            }
         }
     }
 }
