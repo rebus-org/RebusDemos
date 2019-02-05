a92d0b2d7cd2a8478fd53cf10d8352c41e6286ed
Mogens Heller Grabe <mookid8000@gmail.com>
2019-02-05 10:47:52 +0100

    add demo1

---
+++ Demo1/Invoicing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Invoicing.Handlers;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+using Trading.Messages;
+// ReSharper disable ArgumentsStyleNamedExpression
+// ReSharper disable ArgumentsStyleLiteral
+
+namespace Invoicing
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                activator.Register(() => new TradeRecordedHandler());
+
+                var bus = Configure.With(activator)
+                    .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
+                    .Subscriptions(s => s.StoreInSqlServer("server=.; database=RebusDemos; trusted_connection=true", "Subscriptions", isCentralized: true))
+                    .Transport(t => t.UseMsmq("invoicing"))
+                    .Start();
+
+                bus.Subscribe<TradeRecorded>().Wait();
+
+                Console.WriteLine("Invoicing is running - press ENTER to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Demo1/Trading/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Logging;
+using Trading.Messages;
+// ReSharper disable ArgumentsStyleNamedExpression
+
+namespace Trading
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var activator = new BuiltinHandlerActivator())
+            {
+                var bus = Configure.With(activator)
+                    .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
+                    .Subscriptions(s => s.StoreInSqlServer("server=.; database=RebusDemos; trusted_connection=true", "Subscriptions", isCentralized: true))
+                    .Transport(t => t.UseMsmq("trading"))
+                    .Start();
+
+                Console.WriteLine("Trading is running");
+
+                while (true)
+                {
+                    Console.WriteLine("Please enter new trade details");
+                    Console.Write(" commodity > ");
+                    var commodity = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(commodity)) break;
+
+                    Console.Write("  quantity > ");
+                    int quantity;
+                    while (!int.TryParse(Console.ReadLine(), out quantity)) ;
+
+                    bus.Publish(new TradeRecorded(GenerateNewTradeId(), commodity, quantity)).Wait();
+                }
+            }
+
+        }
+
+        static string GenerateNewTradeId() => $"trade-{DateTime.Now:yyyyMMdd-HHmmss}";
+    }
+}
